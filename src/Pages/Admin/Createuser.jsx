e5d60357e47a684b89b2b9ac9555944fd67ad564
mohamed <mohamedmusthaqeem.2101144@srec.ac.in>
2024-01-07 10:59:46 +0530

    create user fixed

--- src/Pages/Admin/Createuser.jsx
+++ src/Pages/Admin/Createuser.jsx
@@ -9,7 +9,6 @@ const Createuser = () => {
     confirmPassword: "",
   });
 
-
   // Handle input changes
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,98 +19,104 @@ const Createuser = () => {
   };
 
   // Handle form submission
-  const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const endpoint = 'http://localhost:8000/user'
+    const endpoint = "http://localhost:8000/user";
     try {
-      const response = await axios.post(endpoint,{
-        "name":formData.username,
-        "email":formData.email,
-        "password":formData.password
-      })
-      console.log(response)
+      const response = await axios.post(endpoint, {
+        name: formData.username,
+        email: formData.email,
+        password: formData.password,
+      });
+      console.log(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
     setFormData({
       username: "",
       email: "",
       password: "",
       confirmPassword: "",
-    })
-  }
+    });
+  };
 
   return (
-    <div className="max-h-screen m-4 w-1/2 space-y-2 bg-green-50">
-      <h1 className="font-Poppins text-2xl text-left ">Create User</h1>
-      <form onSubmit={handleSubmit} className="px-3">
-        <div className="px-3">
-          <label
-            htmlFor="Username"
-            className="block  font-medium text-gray-600"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md outline-none"
-            placeholder="John"
-            required
-          />
-        </div>
-        <div className="px-3">
-          <label htmlFor="email" className="block font-medium text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md outline-none"
-            placeholder="sample@example.com"
-            required
-          />
-        </div>
-        <div className="px-3">
-          <label
-            htmlFor="password"
-            className="block  font-medium text-gray-600"
-          >
-            password
-          </label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md outline-none"
-            placeholder="****"
-            required
-          />
-        </div>
-        <div className="px-3">
-          <label htmlFor="email" className="block font-medium text-gray-600">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md outline-none"
-            placeholder="****"
-            required
-          />
-        </div>
-        {/* {!passwordsMatch && <p>Password and Confirm Password do not match.</p>} */}
-        <button className=" my-2 mx-4 px-6  py-2 bg-Green active:bg-green-600 text-white rounded-md ">
-          Create
-        </button>
-      </form>
+    <div className="min-h-screen p-4 flex w-full justify-center items-center bg-green-50">
+      <div className="lg:w-1/3 h-1/2 border border-Green p-6">
+        <h1 className="font-Poppins text-2xl md:text-3xl text-center text-Green py-2">
+          Create User
+        </h1>
+        <form onSubmit={handleSubmit} className="px-3 space-y-5 md:text-lg">
+          <div className="px-3">
+            <label
+              htmlFor="Username"
+              className="block  font-medium text-gray-600"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md outline-none"
+              placeholder="John"
+              required
+            />
+          </div>
+          <div className="px-3">
+            <label htmlFor="email" className="block font-medium text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md outline-none"
+              placeholder="sample@example.com"
+              required
+            />
+          </div>
+          <div className="px-3">
+            <label
+              htmlFor="password"
+              className="block  font-medium text-gray-600"
+            >
+              password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md outline-none"
+              placeholder="****"
+              required
+            />
+          </div>
+          <div className="px-3">
+            <label htmlFor="email" className="block font-medium text-gray-600">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md outline-none"
+              placeholder="****"
+              required
+            />
+          </div>
+          <div className="flex justify-center">
+            {/* {!passwordsMatch && <p>Password and Confirm Password do not match.</p>} */}
+            <button className=" my-2 mx-4 px-6  py-2 bg-Green text-center  active:bg-green-600 text-white rounded-md ">
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Pages/User/Output.jsx
+++ src/Pages/User/Output.jsx
@@ -1,67 +1,80 @@
-import { useEffect, useState } from "react"
-import { AiOutlineArrowDown } from "react-icons/ai"
-
+import { useEffect, useState } from "react";
+import { FaDownload } from "react-icons/fa6";
 
 const Outputs = [
-    'Output1',
-    'Output2',
-    'Output3',
-    'Output4',
-    'Output5',
-    'Output6',
-    'Output7',
-    'Output8',
-    'Output9',
-    'Output10'
-]
+  "Output1",
+  "Output2",
+  "Output3",
+  "Output4",
+  "Output5",
+  "Output6",
+  "Output7",
+  "Output8",
+  "Output9",
+  "Output10",
+];
 const Output = () => {
-
-    const [outputs,setOutputs] = useState(Outputs)
-    const [search,setSearch] = useState('')
-    const handleChange = (e) =>{
-        setSearch(e.target.value)
+  const [outputs, setOutputs] = useState(Outputs);
+  const [search, setSearch] = useState("");
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  useEffect(() => {
+    if (search === "") {
+      setOutputs(Outputs);
+    } else {
+      setOutputs(
+        outputs.filter((output) => {
+          if (output.toLowerCase().includes(search.toLowerCase()))
+            return output;
+        })
+      );
     }
-    useEffect(()=>{
-        if(search === ''){
-            setOutputs(Outputs)
-        }
-        else{
-            setOutputs(outputs.filter((output)=>{
-                if(output.toLowerCase().includes(search.toLowerCase()))
-                return output
-            }))
-        }
-    },[search,outputs])
+  }, [search, outputs]);
   return (
     <div className="w-full h-screen bg-green-50">
-        <div className="h-fit w-full">
-            <div className="flex justify-between max-md:flex-col items-center px-4 py-2">
-                <div className="text-xl font-semibold">
-                    Output files
-                </div>
-                <div className="flex items-center gap-2">
-                    <input type="text" placeholder="Search" value={search} onChange={handleChange} className="border border-solid border-Green border-opacity-40 rounded-lg px-4 py-1 outline-none" />
-                    <button className="bg-Green text-white px-4 py-1 rounded-lg">Search</button>
-                </div>
+      <div className="h-fit w-full">
+        <div className="flex justify-between max-md:flex-col items-center px-4 py-2">
+          <div className="text-xl font-semibold">Output files</div>
+          <div className="flex items-center gap-2">
+            <input
+              type="text"
+              placeholder="Search"
+              value={search}
+              onChange={handleChange}
+              className="border border-solid border-Green border-opacity-40 rounded-lg px-4 py-1 outline-none"
+            />
+            <button className="bg-Green text-white px-4 py-1 rounded-lg">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="h-5/6">
+        <ul className="p-4 max-h-full overflow-y-auto w-full">
+          {outputs.length === 0 ? (
+            <div className="text-center text-xl font-semibold">
+              No Files Found
             </div>
-        </div>
-        <div className="h-5/6">
-            <ul className="p-4 max-h-full overflow-y-auto w-full">
-                {
-                    outputs.length === 0 ? <div className="text-center text-xl font-semibold">No Files Found</div> :
-                    outputs.map((output,index)=>{
-                        return <li key={index}  className="bg-white flex justify-between shadow-sm border border-solid border-Green border-opacity-40 text-xs h-12 items-center my-2 px-4 w-full rounded-lg">
-                        <span>
-                        {output}
-                        </span>
-                        <button className="border-black border border-solid border-opacity-50 bg-opacity-80 flex items-center text-black font-semibold px-4 py-1 rounded-lg">download<AiOutlineArrowDown className="text-base"/></button>
-                        </li>
-                    })
-                }
-            </ul>
-        </div>
+          ) : (
+            outputs.map((output, index) => {
+              return (
+                <li
+                  key={index}
+                  className="bg-white flex justify-between shadow-sm border border-solid border-Green border-opacity-40 text-xs h-12 items-center my-2 px-4 w-full rounded-lg"
+                >
+                  <span>{output}</span>
+                  <button className="border-black border border-solid border-opacity-50 bg-opacity-80 flex items-center text-black font-semibold px-4 py-1 rounded-lg">
+                    <FaDownload size={15} />
+                  </button>
+                </li>
+              );
+            })
+          )}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Output
+export default Output;
